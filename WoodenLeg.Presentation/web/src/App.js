db03ec61bb85d92c6c4ff191e357a04607cbf8a6
Douglas Breda <bredadouglas@gmail.com>
2019-08-03 18:53:47 +0100

    Added web project using ReactJS

---
+++ WoodenLeg.Presentation/web/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Wooden Leg
+        </a>
+       
+      </nav>
+    </div>
+  );
+}
+
+export default App;
