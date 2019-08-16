31c01167220e94e6efcff438670fd1bb60d173e7
Douglas Breda <bredadouglas@gmail.com>
2019-08-16 22:37:34 +0100

    Developed more parts of the front-end

--- WoodenLeg.Presentation/web/src/pages/announce/Announce.js
+++ WoodenLeg.Presentation/web/src/pages/announce/Announce.js
@@ -17,117 +17,14 @@ export default function Announce() {
               alt="test"
             />
             <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={menuIcon} alt="Details" />
-              </button>
-              <button type="button">
-                <img src={buyIcon} alt="Details" />
-              </button>
-            </div>
-          </li>
-
-          <li>
-            <img
-              src="https://s.glbimg.com/es/sde/f/2019/07/01/ac71f3ebf82fb819599967ea8bff8513_140x140.png"
-              alt="test"
-            />
-            <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={menuIcon} alt="Details" />
-              </button>
-              <button type="button">
-                <img src={buyIcon} alt="Details" />
-              </button>
-            </div>
-          </li>
-
-          <li>
-            <img
-              src="https://s.glbimg.com/es/sde/f/2019/07/01/ac71f3ebf82fb819599967ea8bff8513_140x140.png"
-              alt="test"
-            />
-            <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={menuIcon} alt="Details" />
-              </button>
-              <button type="button">
-                <img src={buyIcon} alt="Details" />
-              </button>
-            </div>
-          </li>
-
-          <li>
-            <img
-              src="https://s.glbimg.com/es/sde/f/2019/07/01/ac71f3ebf82fb819599967ea8bff8513_140x140.png"
-              alt="test"
-            />
-            <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={menuIcon} alt="Details" />
-              </button>
-              <button type="button">
-                <img src={buyIcon} alt="Details" />
-              </button>
-            </div>
-          </li>
-
-          <li>
-            <img
-              src="https://s.glbimg.com/es/sde/f/2019/07/01/ac71f3ebf82fb819599967ea8bff8513_140x140.png"
-              alt="test"
-            />
-            <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={menuIcon} alt="Details" />
-              </button>
-              <button type="button">
-                <img src={buyIcon} alt="Details" />
-              </button>
-            </div>
-          </li>
-
-          <li>
-            <img
-              src="https://s.glbimg.com/es/sde/f/2019/07/01/ac71f3ebf82fb819599967ea8bff8513_140x140.png"
-              alt="test"
-            />
-            <footer>
-              <MdDirectionsRun />
-              <p>Éverton Cebolinha</p>
-              <MdEuroSymbol />
-              <p>1000,00</p>
-              {/* <img className="team-image" src="https://s.glbimg.com/es/sde/f/equipes/2014/04/14/gremio_60x60.png" alt="Team"></img>
-            <p>Grêmio</p> */}
+              <div className="player-info">
+                <MdDirectionsRun />
+                <text>Everton Cebolinha</text>
+              </div>
+              <div className="player-info">
+                <MdEuroSymbol />
+                <text>1000,00</text>
+              </div>
             </footer>
             <div className="buttons">
               <button type="button">
--- WoodenLeg.Presentation/web/src/pages/home/Home.js
+++ WoodenLeg.Presentation/web/src/pages/home/Home.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../../components/navbar";
+import Footer from "../../components/footer";
 
-export default function Home(){
-    return (
-        <h1> This is the home page</h1>
-    );
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>This is the home page</h1>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
